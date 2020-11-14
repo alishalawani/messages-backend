b377787def2f1e506b66b9df79be57620c3e7468
alishalawani <alishalawani@gmail.com>
2020-11-14 02:44:54 -0600

    created get and post routes

--- controllers/messages.js
+++ controllers/messages.js
@@ -3,3 +3,31 @@ const Message = require('../db/models/Message');
 const router = express.Router();
 
 //Get all messages
+router.get('/', (req, res) => {
+	Message.find({})
+		.then((messages) => {
+			res.json(messages);
+		})
+		.catch(console.error);
+});
+
+//get message by id
+router.get('/:id', (req, res) => {
+	Message.findById({ _id: req.params.id })
+		.then((message) => {
+			res.json(message);
+		})
+		.catch(console.error);
+});
+
+//create a message
+router.post('/', (req, res) => {
+	const newMessage = req.body;
+	Message.create(newMessage)
+		.then((message) => {
+			res.json(message);
+		})
+		.catch(console.error);
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const messagesController = require('./controllers/messages');
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
