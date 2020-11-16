3d98d2ab7b0779e9d60237176fc6866c297c1d2f
alishalawani <alishalawani@gmail.com>
2020-11-15 22:17:28 -0600

    set up service worker

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
 const express = require('express');
 const cors = require('cors');
+const webpush = require('web-push');
+const bodyParser = require('body-parser');
+const path = require('path');
 
 const messagesController = require('./controllers/messages');
 
 const app = express();
+
+app.use(express.static(path.join(_dirname, 'client')));
+
+
+app.use(bodyParser.json());
+
+
+
+const privateVapidKey = process.env.PRIVATE_VAPID_KEY;
+const publicVapidKey = process.env.PUBLIC_VAPID_KEY;
+
+webpush.setVapidDetails(
+	'mailto:alishalawani@gmail.com',
+	publicVapidKey,
+	privateVapidKey
+);
+console.log(privateVapidKey)
+//subscribe route ..sends the notification to the service worker
+app.post('/subscribe', (req, res)=>{
+	//Get pushSubscription object
+	const subscription = req.body;
+
+	//send a 201 status that shows the the resource has been created
+	res.status(201).json({})
+
+	//create payload
+	const payload = JSON.stringify({title: 'Push Test'});
+
+	//Pass object into sendNotification
+	webpush.sendNotification(subscription, payload).catch(console.error);
+})
 
 app.use(cors());
 
